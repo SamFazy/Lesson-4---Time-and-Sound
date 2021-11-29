6423c6935983636cd33b7d2cce63acde69738b20
Sam F <samfaz10@hotmail.com>
2021-11-29 14:41:34 -0500

    1

--- Lesson 4 - Time and Sound/Game1.cs
+++ Lesson 4 - Time and Sound/Game1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -9,6 +10,18 @@ namespace Lesson_4___Time_and_Sound
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        MouseState mouseState;
+
+        Texture2D bombTexture;
+        Rectangle bombRect;
+
+        float seconds;
+        float startTime;
+
+        SoundEffect explode;
+
+        SpriteFont timeFont;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -18,9 +31,20 @@ namespace Lesson_4___Time_and_Sound
 
         protected override void Initialize()
         {
+            this.Window.Title = "Time and Sound";
+
             // TODO: Add your initialization logic here
+            bombRect = new Rectangle(50, 50, 700, 400);
+
+
+            // Resize Window
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 500;
+            _graphics.ApplyChanges();
 
             base.Initialize();
+
+            
         }
 
         protected override void LoadContent()
@@ -28,10 +52,38 @@ namespace Lesson_4___Time_and_Sound
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            bombTexture = Content.Load<Texture2D>("bomb");
+            timeFont = Content.Load<SpriteFont>("Time");
+            explode = Content.Load<SoundEffect>("explosion");
         }
 
         protected override void Update(GameTime gameTime)
         {
+            mouseState = Mouse.GetState();
+
+            seconds = 15 - (float)gameTime.TotalGameTime.TotalSeconds + startTime;
+
+            if (seconds < 0)
+            {
+                seconds = 0;
+            }
+
+            if (gameTime.TotalGameTime.TotalSeconds > 15 && gameTime.TotalGameTime.TotalSeconds < 15.01)
+            {
+                explode.Play();
+            }
+
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                startTime = (float)gameTime.TotalGameTime.TotalSeconds;
+            }
+
+            if (gameTime.TotalGameTime.TotalSeconds > 24)
+            {
+                Exit();
+            }
+
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
@@ -45,6 +97,12 @@ namespace Lesson_4___Time_and_Sound
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(bombTexture, bombRect, Color.White);
+            _spriteBatch.DrawString(timeFont, seconds.ToString("00.0"), new Vector2(270, 200), Color.Black);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
